04b18cd08eb34327c47ae7d5c16620db9aeb5e3f
Alexr16 <j.alex-1996@hotmail.com>
2022-04-28 12:00:05 -0500

    Create books data

--- src/pages/Books.js
+++ src/pages/Books.js
@@ -1,6 +1,31 @@
 import React from 'react';
+import BooksList from '../components/BooksList';
 
-const Books = () => (
-  <div className="sooks-container" />
-);
+const Books = () => {
+  const books = [
+    {
+      id: 1,
+      title: 'Six of Crows',
+      author: 'Leigh Bardugo',
+    },
+    {
+      id: 2,
+      title: 'Every Day',
+      author: 'David Levithan',
+    },
+    {
+      id: 3,
+      title: 'Darius the Great Is Not Okay',
+      author: 'Adib Khorram',
+    },
+  ];
+
+  return (
+    <div className="books-container">
+      <BooksList
+        books={books}
+      />
+    </div>
+  );
+};
 export default Books;
