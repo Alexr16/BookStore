b19c5f7dd8ddeed8590491e37ef3440485a2e990
Alexr16 <j.alex-1996@hotmail.com>
2022-04-28 10:32:48 -0500

    Create Navbar including Navlinks

---
+++ src/components/Navbar.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { NavLink, Outlet } from 'react-router-dom';
+
+const Navbar = () => (
+  <div>
+    <nav style={{
+      padding: '1rem',
+      display: 'flex',
+      justifyContent: 'space-between',
+    }}
+    >
+      <div className="logo-conatiner">
+        <h1>Bookstore CMS</h1>
+        <ul style={{ display: 'flex', alignItems: 'center' }}>
+          <li
+            className="li-navbar"
+            key={1}
+          >
+            <NavLink
+              style={({ isActive }) => ({
+                display: 'block',
+                textDecoration: 'none',
+                color: isActive ? 'blue' : 'black',
+              })}
+              to="/books"
+              exact
+            >
+              Books
+            </NavLink>
+          </li>
+          <li
+            className="li-navbar"
+            key={2}
+          >
+            <NavLink
+              style={({ isActive }) => ({
+                display: 'block',
+                textDecoration: 'none',
+                color: isActive ? 'blue' : 'black',
+              })}
+              to="/categories"
+              exact
+            >
+              Categories
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+      <div className="user">Bookstore CMS</div>
+    </nav>
+    <Outlet />
+  </div>
+);
+
+export default Navbar;
