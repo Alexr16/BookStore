28f0b6405687adf8944258b0a285160b8cb8c951
Alexr16 <j.alex-1996@hotmail.com>
2022-05-03 13:48:31 -0500

    Create add new book functionality using useDispatch

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,14 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
+import { addBook } from '../redux/books/books';
 
-const Form = () => (
-  <>
-    <div className="form">
-      <input placeholder="Book title" className="title" />
-      <input placeholder="Book author" className="author" />
-      <button type="submit" label="Add" className="add-book">ADD BOOK</button>
-    </div>
+const Form = () => {
+  const dispatch = useDispatch();
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
 
-  </>
-);
+  const submit = (e) => {
+    e.preventDefault();
+    if (title !== '' || author !== '') {
+      const Book = {
+        id: uuidv4(),
+        title,
+        author,
+      };
+      dispatch(addBook(Book));
+    }
+    setTitle('');
+    setAuthor('');
+  };
+
+  return (
+    <>
+      <form className="form" onSubmit={submit}>
+        <input placeholder="Book title" className="title" onChange={(e) => setTitle(e.target.value)} value={title} />
+        <input placeholder="Book author" className="author" onChange={(e) => setAuthor(e.target.value)} value={author} />
+        <button type="submit" label="Add" className="add-book">ADD BOOK</button>
+      </form>
+
+    </>
+  );
+};
 
 export default Form;
