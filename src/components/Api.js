2d9229f4976f194ab6a41a0d3e29a1cc5d3fe1de
Alexr16 <j.alex-1996@hotmail.com>
2022-05-04 10:26:54 -0500

    Add Api calls

---
+++ src/components/Api.js
@@ -0,0 +1,31 @@
+const URL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/DYS9eNOK7ZqdehV5UTfN/books';
+
+const createBook = async (id, bookTitle, author) => {
+  const response = await fetch(URL, {
+    method: 'POST',
+    body: JSON.stringify({
+      item_id: id,
+      title: bookTitle,
+      author,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=utf-8',
+    },
+  });
+  const data = await response.text();
+  return data;
+};
+
+const removeBook = async (id) => {
+  const response = await fetch(`${URL}/${id}`, {
+    method: 'DELETE',
+    body: JSON.stringify({
+      item_id: id,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=utf-8',
+    },
+  });
+};
+
+export { createBook, removeBook };
